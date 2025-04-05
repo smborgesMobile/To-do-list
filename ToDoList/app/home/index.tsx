2bf583b28ee39026cb5997aac86d29f9c5f15404
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-04-05 19:58:36 -0300

    feat: update HomeScreen layout with logo and remove unused components

--- ToDoList/app/home/index.tsx
+++ ToDoList/app/home/index.tsx
@@ -1,11 +1,16 @@
-import { Text } from 'react-native';
+import { Image, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from "./styles";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Essa é a minha home</Text>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../../assets/images/logo.png')}
+          style={styles.logo}
+        />
+      </View>
     </SafeAreaView>
   );
 }
--- ToDoList/app/home/styles.tsx
+++ ToDoList/app/home/styles.tsx
@@ -3,12 +3,20 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 20,
+        backgroundColor: "#0D0D0D",
+        paddingTop: 24
     },
     link: {
         marginTop: 15,
         paddingVertical: 15,
+    },
+    logo: {
+        width: 110,
+        height: 32,
+        alignItems: "center"
+    },
+    logoContainer: {
+        flex: 1,
+        alignItems: 'center',
     },
 })
