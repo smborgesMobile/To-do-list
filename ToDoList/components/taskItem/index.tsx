0eb97620d1e5f86d27830481bffd981a5b69b0d5
Sérgio Borges <sergioborges.ciandt@natura.net>
2025-04-06 00:03:33 -0300

    feat: enhance TaskItem component with checkbox state management

--- ToDoList/components/taskItem/index.tsx
+++ ToDoList/components/taskItem/index.tsx
@@ -1,16 +1,23 @@
+import React, { useState } from "react";
 import { View, Text, Image } from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { styles } from "./styles";
 
 export function TaskItem() {
+    // Defina o estado do checkbox
+    const [isChecked, setIsChecked] = useState(false);
+
     return (
         <View style={styles.container}>
             <BouncyCheckbox
                 size={20}
-                fillColor="#4EA8DE"
+                fillColor={isChecked ? "#5E60CE" : "#4EA8DE"} 
                 iconStyle={{ borderColor: "blue" }}
                 innerIconStyle={{ borderWidth: 3 }}
-                onPress={(isChecked: boolean) => { console.log(isChecked) }}
+                onPress={(checked: boolean) => { 
+                    setIsChecked(checked); 
+                    console.log(checked); 
+                }}
             />
             <Text
                 numberOfLines={3}
